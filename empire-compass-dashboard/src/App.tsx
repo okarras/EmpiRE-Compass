426c4294a6f2821154086a13b47af833225b5a98
amirreza alasti <amirrezaalasti@amirrezas-MacBook-Pro.local>
2025-01-29 18:43:03 +0100

    added dashboard

--- empire-compass-dashboard/src/App.tsx
+++ empire-compass-dashboard/src/App.tsx
@@ -1,9 +1,50 @@
-function App() {
+import Dashboard from "./components/dashboard";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
+function App() {
   return (
-    <>
-    </>
-  )
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        minHeight: "100vh",
+        backgroundColor: "#F8F8F8",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {/* Header Section */}
+      <Box
+        sx={{
+          width: "98%",
+          backgroundColor: "#e86161",
+          padding: "16px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography variant="h6" sx={{ color: "white", fontWeight: "bold" }}>
+        EmpiRE-Compass Dashboard
+        </Typography>
+      </Box>
+      
+      {/* Main Content Section */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          flexGrow: 1,
+        }}
+      >
+        <Dashboard />
+      </Box>
+    </Box>
+  );
 }
 
-export default App
+export default App;
---
+++ empire-compass-dashboard/src/components/Dashboard.tsx
@@ -0,0 +1,55 @@
+import { BarChart } from '@mui/x-charts/BarChart';
+import data from '../../data/sample_data.json';
+import { axisClasses } from '@mui/x-charts/ChartsAxis';
+import Box from '@mui/material/Box';
+
+
+const Dashboard = () => {
+    //sort the data by year
+    data.sort((a, b) => a.year - b.year);
+ // get the unique years from the data
+    const years = [...new Set(data.map((item) => item.year))];
+    console.log(years);
+    // get number of items for each year
+    const itemsPerYear = years.map((year, index) => {
+        return {count: data.filter((item) => item.year === year).length, year: year};
+    });
+    console.log(itemsPerYear);
+
+    const chartSetting = {
+        yAxis: [
+          {
+            label: 'number of papers',
+          },
+        ],
+        series: [{ dataKey: 'count' }],
+        height: 300,
+        sx: {
+          [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
+            transform: 'translateX(-10px)',
+          },
+        },
+      };
+  return (
+    <Box sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '90%',
+        backgroundColor: '#FFFFFF',
+        borderRadius: '10px',
+        padding: '20px',
+    }}>
+        <BarChart
+            dataset={itemsPerYear}
+            xAxis={[
+                { scaleType: 'band', dataKey: 'year', valueFormatter: (v) => v.toString(), tickPlacement: "middle" },
+            ]}
+            {...chartSetting}
+            colors={['#e86161']}
+        />
+    </Box>
+  )
+}
+
+export default Dashboard
