2ec00a4bd535a684b84538cc3b84683b62ded255
amirreza alasti <amirrezaalasti@dyn-140240.mip.uni-hannover.de>
2025-02-03 11:22:39 +0100

    added eslint and prettier

--- empire-compass-dashboard/src/App.tsx
+++ empire-compass-dashboard/src/App.tsx
@@ -1,43 +1,43 @@
-import Dashboard from "./components/dashboard";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import Dashboard from './components/Dashboard';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <Box
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
-        minHeight: "100vh",
-        backgroundColor: "#F8F8F8",
-        justifyContent: "center",
-        alignItems: "center",
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        minHeight: '100vh',
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       {/* Header Section */}
       <Box
         sx={{
-          width: "98%",
-          backgroundColor: "#e86161",
-          padding: "16px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
+          width: '98%',
+          backgroundColor: '#e86161',
+          padding: '16px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
         }}
       >
-        <Typography variant="h6" sx={{ color: "white", fontWeight: "bold" }}>
-        EmpiRE-Compass Dashboard
+        <Typography variant="h6" sx={{ color: 'white', fontWeight: 'bold' }}>
+          EmpiRE-Compass Dashboard
         </Typography>
       </Box>
-      
+
       {/* Main Content Section */}
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
           flexGrow: 1,
         }}
       >
--- empire-compass-dashboard/src/components/Dashboard.tsx
+++ empire-compass-dashboard/src/components/Dashboard.tsx
@@ -3,35 +3,38 @@ import data from '../../data/sample_data.json';
 import { axisClasses } from '@mui/x-charts/ChartsAxis';
 import Box from '@mui/material/Box';
 
-
 const Dashboard = () => {
-    //sort the data by year
-    data.sort((a, b) => a.year - b.year);
- // get the unique years from the data
-    const years = [...new Set(data.map((item) => item.year))];
-    console.log(years);
-    // get number of items for each year
-    const itemsPerYear = years.map((year, index) => {
-        return {count: data.filter((item) => item.year === year).length, year: year};
-    });
-    console.log(itemsPerYear);
+  //sort the data by year
+  data.sort((a, b) => a.year - b.year);
+  // get the unique years from the data
+  const years = [...new Set(data.map((item) => item.year))];
+  console.log(years);
+  // get number of items for each year
+  const itemsPerYear = years.map((year) => {
+    return {
+      count: data.filter((item) => item.year === year).length,
+      year: year,
+    };
+  });
+  console.log(itemsPerYear);
 
-    const chartSetting = {
-        yAxis: [
-          {
-            label: 'number of papers',
-          },
-        ],
-        series: [{ dataKey: 'count' }],
-        height: 300,
-        sx: {
-          [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
-            transform: 'translateX(-10px)',
-          },
-        },
-      };
+  const chartSetting = {
+    yAxis: [
+      {
+        label: 'number of papers',
+      },
+    ],
+    series: [{ dataKey: 'count' }],
+    height: 300,
+    sx: {
+      [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
+        transform: 'translateX(-10px)',
+      },
+    },
+  };
   return (
-    <Box sx={{
+    <Box
+      sx={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -39,17 +42,23 @@ const Dashboard = () => {
         backgroundColor: '#FFFFFF',
         borderRadius: '10px',
         padding: '20px',
-    }}>
-        <BarChart
-            dataset={itemsPerYear}
-            xAxis={[
-                { scaleType: 'band', dataKey: 'year', valueFormatter: (v) => v.toString(), tickPlacement: "middle" },
-            ]}
-            {...chartSetting}
-            colors={['#e86161']}
-        />
+      }}
+    >
+      <BarChart
+        dataset={itemsPerYear}
+        xAxis={[
+          {
+            scaleType: 'band',
+            dataKey: 'year',
+            valueFormatter: (v) => v.toString(),
+            tickPlacement: 'middle',
+          },
+        ]}
+        {...chartSetting}
+        colors={['#e86161']}
+      />
     </Box>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
