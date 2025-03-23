c85cb34e8263feadaf51196c66e33b1697ad31d3
sushantaggarwal <sushant.aggarwal@finverv.com>
2025-03-23 22:37:55 +0100

    code style changes

--- empire-compass-dashboard/src/components/Dashboard.tsx
+++ empire-compass-dashboard/src/components/Dashboard.tsx
@@ -3,6 +3,8 @@ import CustomBarChart from './CustomCharts/CustomBarChart';
 import { useEffect, useState } from 'react';
 import fetchSPARQLData from '../helpers/fetch_query';
 import { query_1 } from '../queries/queries';
+import { query_2_1 } from '../queries/queries';
+import { query_3 } from '../queries/queries';
 import { queries } from '../constants/queries_chart_info';
 import AllCharts from './CustomCharts2/AllCharts'
 import DisplayQuestions from './DisplayQuestions';
@@ -25,25 +27,36 @@ const Dashboard = () => {
     query_1: [],
   });
 
+
   useEffect(() => {
     fetchSPARQLData(query_1).then((data) => {
       setOrkgData((prevData) => ({ ...prevData, query_1: data }));
     });
-    
+    fetchSPARQLData(query_2_1).then((data) => {
+      setOrkgData((prevData) => ({ ...prevData, query_2_1: data }));
+    });
+    fetchSPARQLData(query_3).then((data) => {
+      setOrkgData((prevData) => ({ ...prevData, query_3: data }));
+    });
+    console.log(orkgData)
   }, []);
+
+  useEffect(()=>{
+    console.log(typeof(orkgData));
+  }, [orkgData])
 
   return (
     <>
-      {AllCharts().map((value, index)=>{
+      {/* {AllCharts().map((value, index)=>{
         let Module = value.ChartComponent;
         return Module ? <>
         <DisplayQuestions key={"allCharts" + index} question={value.question} >
           <Module/>
         </DisplayQuestions>
       </> : <> </>
-      })} 
-      
-      {queries.map((query, index) => (
+      })}  */}
+
+      { Object.keys(orkgData).length > 1 ? queries.map((query, index) => (
         <Question key={index} dataAnalysisInformation={query.dataAnalysisInformation}>
           {/* TODO: for each query, the chart can be different */}
           <CustomBarChart
@@ -53,7 +66,9 @@ const Dashboard = () => {
             chartSetting={query.chartSetting}
           />
         </Question>
-      ))}
+      )) : <>  </> }
+
+      {}
     </>
   );
 };
--- empire-compass-dashboard/src/constants/queries_chart_info.ts
+++ empire-compass-dashboard/src/constants/queries_chart_info.ts
@@ -1,4 +1,6 @@
 import { axisClasses } from '@mui/x-charts';
+// import questions2Data from '../../data/query_2.1_data_2024-07-26.json'
+// import questions3Data from '../../data/query_3_data_2025-02-27.json'
 
 export const queries = [
   // Query 1
@@ -39,6 +41,121 @@ export const queries = [
     },
     dataAnalysisInformation:{
       question: 'How has the proportion of empirical studies evolved over time?'
+    }
+  },
+  {
+    title: 'Number of papers per year',
+    id: 2_1,
+    uid: 'query_2_1',
+    chartSetting: {
+      yAxis: [
+        {
+          label: 'Number of empirical methods used',
+        },
+      ],
+      series: [
+        { dataKey: 'case study', label: 'case study' },
+        { dataKey: 'experiment', label: 'Experiment' },
+        { dataKey: 'survey', label: 'Survey' },
+        { dataKey: 'interview', label: 'Interview' },
+        { dataKey: 'secondary research', label: 'Secondary research' },
+        { dataKey: 'action research', label: 'action research' },
+        { dataKey: 'others', label: 'Other' }
+      ],
+      height: 300,
+      sx: {
+        [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
+          transform: 'translateX(-10px)',
+        },
+      },
+    },
+    dataProcessingFunction: (SampleData) => {
+      //sort the data by year
+      let years = [...new Set(SampleData.map((item: { year: unknown }) => item.year))];
+      years = years.map((value)=>{
+        return {"year": value}
+      })
+
+      let processedData = years.map((value, index)=>{
+
+        let filteredData = SampleData.filter((dataValue)=>{
+          return dataValue.year == value.year
+        })
+        let result = {"year":value.year, "total": 0};
+
+        filteredData.forEach((value: {dc_method_type_label: string}, index)=>{
+          if (value.dc_method_type_label == "action research" || value.dc_method_type_label == "case study" || value.dc_method_type_label == "experiment"|| value.dc_method_type_label == "interview" || value.dc_method_type_label == "secondary research" || value.dc_method_type_label == "survey" || value.dc_method_type_label == "secondary research" ) {
+            if (result.hasOwnProperty(value.dc_method_type_label)) {
+              result[value.dc_method_type_label] = result[value.dc_method_type_label] + 1;
+            } else{
+              result[value.dc_method_type_label] = 1;
+            }
+          } else {
+            if (result.hasOwnProperty("others")) {
+              result.others = result.others + 1;
+            } else {
+              result.others = 1
+            }
+          }
+
+          result.total = result.total + 1;
+        })
+
+        Object.entries(result).forEach((value, index)=>{
+          if (value[0] != "year" && value[0] != "total" ) {
+            result[value[0] + " ratio"] = Number((value[1]/result.total).toFixed(2));
+          }
+        })
+        return result;
+      })
+      processedData.sort((a,b)=>{ return a.year - b.year });
+
+      return processedData;
+    },
+    dataAnalysisInformation:{
+      question: 'How often are which empirical methods used over time?'
+    }
+  },
+  {
+    title: 'Number of papers per year',
+    id: 3,
+    uid: 'query_3',
+    chartSetting: {
+      yAxis: [
+        {
+          label: 'Number of empirical methods used',
+        },
+      ],
+      series: [
+        { dataKey: 'count', label: 'Case study' },
+      ],
+      height: 300,
+      sx: {
+        [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
+          transform: 'translateX(-10px)',
+        },
+      },
+    },
+    dataProcessingFunction: (SampleData) => {
+
+      const dataYears = [
+        ...new Set(SampleData.map((item: { year: number }) => item.year)),
+      ];
+
+      const itemsPerYear = dataYears.map((year) => {
+        return {
+          count: SampleData.filter((item: { year: number, dc_label: string, da_label: string }) => item.year === year && item.dc_label == 'collection' && item.da_label == 'analysis' ).length,
+          ratio: Number(((SampleData.filter((item: { year: number, dc_label: string, da_label: string }) => item.year === year ).length - SampleData.filter((item: { year: number, dc_label: string, da_label: string }) => item.year === year && item.dc_label == 'collection' && item.da_label == 'analysis' ).length ) / SampleData.filter((item: { year: number, dc_label: string, da_label: string }) => item.year === year ).length).toFixed(2)),
+          year: year,
+        };
+      });
+
+      itemsPerYear.sort((a,b)=>{ return a.year - b.year });
+
+      return itemsPerYear;
+    },
+    dataAnalysisInformation:{
+      question: 'How has the proportion of papers that do not have an empirical study evolved over time?'
     }
   },
 ];
