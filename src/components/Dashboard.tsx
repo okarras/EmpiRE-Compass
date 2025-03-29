24cf7cf2bf779a8912f1eb15edfd8e9ef7a9eb55
Amirreza Alasti <amirrezaalasti@gmail.com>
2025-03-29 11:25:45 +0100

    fix dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,39 +1,43 @@
-// import { useEffect, useState } from 'react';
-// import fetchSPARQLData from '../helpers/fetch_query';
-// import { SPARQL_QUERIES } from '../api/SPARQL_QUERIES';
+import Question from './Question';
+import CustomBarChart from './CustomCharts/CustomBarChart';
+import { useEffect, useState } from 'react';
+import fetchSPARQLData from '../helpers/fetch_query';
+import { SPARQL_QUERIES } from '../api/SPARQL_QUERIES';
+import { queries } from '../constants/queries_chart_info';
 import { Box } from '@mui/system';
+import { Divider } from '@mui/material';
 
-// interface OrkgData {
-//   [key: string]: unknown[]; // Make this dynamic, allowing different keys for each query
-// }
+interface OrkgData {
+  [key: string]: unknown[]; // Make this dynamic, allowing different keys for each query
+}
 
 const Dashboard = () => {
-  // const [orkgData, setOrkgData] = useState<OrkgData>({});
+  const [orkgData, setOrkgData] = useState<OrkgData>({});
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const dataEntries = await Promise.all(
-  //       Object.keys(SPARQL_QUERIES).map(async (query) => {
-  //         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  //         //@ts-expect-error
-  //         const data = await fetchSPARQLData(SPARQL_QUERIES[query]);
-  //         return { [query]: data };
-  //       })
-  //     );
+  useEffect(() => {
+    const fetchData = async () => {
+      const dataEntries = await Promise.all(
+        Object.keys(SPARQL_QUERIES).map(async (query) => {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          //@ts-expect-error
+          const data = await fetchSPARQLData(SPARQL_QUERIES[query]);
+          return { [query]: data };
+        })
+      );
 
-  //     // Merge all fetched data into a single object
-  //     const newData = Object.assign({}, ...dataEntries);
+      // Merge all fetched data into a single object
+      const newData = Object.assign({}, ...dataEntries);
 
-  //     // Update state properly
-  //     setOrkgData(newData);
-  //   };
+      // Update state properly
+      setOrkgData(newData);
+    };
 
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
 
-  // if (Object.keys(orkgData).length === 0) {
-  //   return <div>Loading...</div>;
-  // }
+  if (Object.keys(orkgData).length === 0) {
+    return <div>Loading...</div>;
+  }
   return (
     <Box
       sx={{
@@ -45,7 +49,38 @@ const Dashboard = () => {
         flexDirection: 'column',
       }}
     >
-
+      {queries.map((query, index) => (
+        <>
+          <Question
+            key={index}
+            dataAnalysisInformation={query.dataAnalysisInformation}
+          >
+            {/* TODO: for each query, the chart can be different */}
+            <>
+              {query.chartSettings.map((chart, index) => {
+                return (
+                  <CustomBarChart
+                    key={index}
+                    question_id={query.uid}
+                    dataset={
+                      query.dataProcessingFunction(
+                        orkgData[query.uid] as { year: number }[]
+                      ) ?? []
+                    } // Cast the dynamic value to unknown[] to match the expected type
+                    chartSetting={chart}
+                  />
+                );
+              })}
+            </>
+          </Question>
+          <Divider
+            variant="fullWidth"
+            sx={{
+              backgroundColor: 'black',
+            }}
+          />
+        </>
+      ))}
     </Box>
   );
 };
