07d5a9fa2035bab883ce9f60b90ead08e17b381c
Amirreza Alasti <amirrezaalasti@gmail.com>
2025-05-15 21:40:55 +0200

    added piechart

--- src/components/CustomCharts/CustomPieChart.tsx
+++ src/components/CustomCharts/CustomPieChart.tsx
@@ -1,5 +1,29 @@
-const CustomPieChart = () => {
-  return <div>CustomPieChart</div>;
+import { PieChart } from '@mui/x-charts/PieChart';
+import { ChartSetting } from '../../constants/queries_chart_info';
+
+interface CustomPieChartInterface {
+  dataset: Array<{
+    [key: string]: string | number;
+  }>;
+  chartSetting: ChartSetting;
+  question_id: string;
+  loading: boolean;
+}
+
+const CustomPieChart = (props: CustomPieChartInterface) => {
+  const { dataset, chartSetting, question_id, loading } = props;
+  console.log('CustomPieChart', chartSetting, dataset);
+
+  return (
+    <div className={chartSetting.className} id={`chart-${question_id}`}>
+      <h4 style={{ textAlign: 'center' }}> {chartSetting.heading} </h4>
+      <PieChart
+        colors={chartSetting.colors ?? ['#e86161']}
+        loading={loading}
+        {...chartSetting}
+      />
+    </div>
+  );
 };
 
 export default CustomPieChart;
