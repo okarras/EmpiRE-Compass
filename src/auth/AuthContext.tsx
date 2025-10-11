717a508aeda264201f680487debf70e4f12aaf30
Amirreza Alasti <amirrezaalasti@gmail.com>
2025-10-11 09:57:21 +0200

    fix: change keycloak conditions to be initialized

--- src/auth/AuthContext.tsx
+++ src/auth/AuthContext.tsx
@@ -26,7 +26,7 @@ const AuthContextProvider: React.FC<AuthProviderProps> = ({ children }) => {
 
   // Save user data to Firebase when authenticated
   useEffect(() => {
-    if (!initialized || location.pathname !== '/statistics') {
+    if (!initialized || location.pathname === '/statistics') {
       return;
     }
 
