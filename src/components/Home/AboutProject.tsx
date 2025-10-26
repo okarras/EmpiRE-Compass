1a9499053dff7e0e85681f3cf0136b6923a9a3cc
Amirreza Alasti <amirrezaalasti@gmail.com>
2025-10-26 12:59:59 +0100

    feat: process html in home

--- src/components/Home/AboutProject.tsx
+++ src/components/Home/AboutProject.tsx
@@ -32,44 +32,13 @@ const AboutProject = ({ content }: AboutProjectProps) => {
       >
         {content.title}
       </Typography>
-      {content.content.split('\n\n').map((paragraph, index) => (
-        <Typography
-          key={index}
-          paragraph
-          sx={{
-            fontSize: { xs: '1rem', sm: '1.1rem' },
-            lineHeight: 1.7,
-            color: 'text.primary',
-            mb: index === 0 ? 3 : 2,
-          }}
-        >
-          {paragraph}
-        </Typography>
-      ))}
+
+      <Box dangerouslySetInnerHTML={{ __html: content.content }} />
+
       <Box sx={{ pl: { xs: 2, sm: 3, md: 4 } }}>
         <Typography component="ul" sx={{ listStyle: 'none', p: 0 }}>
           {content.themes.map((theme) => (
-            <Typography
-              component="li"
-              key={theme}
-              sx={{
-                mb: 1.5,
-                fontSize: { xs: '1rem', sm: '1.1rem' },
-                display: 'flex',
-                alignItems: 'center',
-                '&:before': {
-                  content: '""',
-                  width: '8px',
-                  height: '8px',
-                  backgroundColor: '#e86161',
-                  borderRadius: '50%',
-                  display: 'inline-block',
-                  mr: 2,
-                },
-              }}
-            >
-              {theme}
-            </Typography>
+            <Box dangerouslySetInnerHTML={{ __html: theme }} />
           ))}
         </Typography>
       </Box>
