4d84caa5356bc8e0be6c8eddb889d437f571cf80
yucelclkk <yucelclkk@gmail.com>
2025-11-03 00:22:51 +0100

    fix: missing property

--- src/context/DynamicQuestionContext.tsx
+++ src/context/DynamicQuestionContext.tsx
@@ -37,6 +37,7 @@ export interface DynamicQuestionHistory {
 const initialState: DynamicQuestionState = {
   question: '',
   sparqlQuery: '',
+  sparqlTranslation: '',
   queryResults: [],
   chartHtml: '',
   questionInterpretation: '',
